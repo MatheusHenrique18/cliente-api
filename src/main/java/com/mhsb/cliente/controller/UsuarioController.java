57a4ba0ce6e1992406c6b11f2d26e17f514e44a9
MatheusHenrique18 <matheus.hsb18@gmail.com>
2021-09-13 16:33:48 -0300

    incluindo usuarios default caso a lista esteja vazia, e já retornando os mesmos na listagem de usuarios.

--- src/main/java/com/mhsb/cliente/controller/UsuarioController.java
+++ src/main/java/com/mhsb/cliente/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package com.mhsb.cliente.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.mhsb.cliente.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping("api/usuario")
+@CrossOrigin(origins = "*")
 public class UsuarioController {
 	
 	@Autowired
@@ -19,6 +21,25 @@ public class UsuarioController {
 	
 	@GetMapping
 	private List<Usuario> findAll(){
+		
+		List<Usuario> usuariosCadastrados = usuarioRepository.findAll();
+		
+		if(usuariosCadastrados.size() == 0) {
+			Usuario usuAdmin = new Usuario();
+			usuAdmin.setNome("admin");
+			usuAdmin.setSenha("123456");
+			usuAdmin.setPerfil("ADMIN");
+			Usuario usuComum = new Usuario();
+			usuComum.setNome("comum");
+			usuComum.setSenha("123456");
+			usuComum.setPerfil("COMUM");
+			
+			usuarioRepository.save(usuAdmin);
+			usuarioRepository.save(usuComum);
+		}else {
+			return usuariosCadastrados;
+		}
+		
 		return usuarioRepository.findAll();
 	}
 	
